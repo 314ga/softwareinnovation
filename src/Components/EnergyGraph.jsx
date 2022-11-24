03cb7c6eafc13255031c3b9e7d566ba988ef5b2a
Maria4lexzy <37138736+Maria4lexzy@users.noreply.github.com>
2022-11-25 00:58:00 +0100

    update api fetching data and graph display

--- src/Components/EnergyGraph.jsx
+++ src/Components/EnergyGraph.jsx
@@ -3,24 +3,33 @@ import { Chart } from "react-google-charts";
 // import client from "../api/energiApi";
 
 const EnergyGraph = (props) => {
+  const [energyPrices, setEnergyPrices] = useState([]);
   let today = new Date();
   let dd = String(today.getDate()).padStart(2, "0");
   let mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
   let yyyy = today.getFullYear();
-  let minute = today.getHours();
-  let hour = today.getMinutes();
-  let startDate = yyyy + "-" + mm + "-" + dd + "T" + hour + ":" + minute;
-  let endDate = yyyy + "-" + mm + "-" + (dd + 1) + "T" + hour + ":" + minute;
-  const [energyPrices, setEnergyPrices] = useState([]);
-
+  let month = today.toLocaleDateString("default", { month: "long" });
+  let tomorrow = String(today.getDate() + 1).padStart(2, "0");
+  let startDate = yyyy + "-" + mm + "-" + dd;
+  let endDate = yyyy + "-" + mm + "-" + tomorrow;
   useEffect(() => {
     setEnergyPrices(props.priceData);
   }, []);
 
   return (
-    <div className="app__energy">
+    <div className="app__energy ">
       <h2 className="display-2">Today's Prices</h2>
       <p className="lead">Prices on the graph are Price per kWh</p>
+      <p className="text-info bg-dark p-2 w-50 mx-auto ">
+        {"Displaying data from " +
+          dd +
+          " " +
+          month +
+          " to " +
+          tomorrow +
+          " " +
+          month}
+      </p>
       {energyPrices.length > 0 ? (
         <div>
           <Chart
--- src/api/energiApi.js
+++ src/api/energiApi.js
@@ -1,15 +1,15 @@
 import axios from "axios";
-var testy = "Elspotprices";
 let today = new Date();
 let dd = String(today.getDate()).padStart(2, "0");
 let mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
 let yyyy = today.getFullYear();
-let minute = today.getHours();
-let hour = today.getMinutes();
-let startDate = yyyy + "-" + mm + "-" + (dd - 2) + "T" + hour + ":" + minute;
-let endDate = yyyy + "-" + mm + "-" + (dd - 1) + "T" + hour + ":" + minute;
+
+let tomorrow = String(today.getDate() + 1).padStart(2, "0");
+let startDate = yyyy + "-" + mm + "-" + dd;
+let endDate = yyyy + "-" + mm + "-" + tomorrow;
+
 const client = axios.create({
-  baseURL: `https://api.energidataservice.dk/dataset/Elspotprices?offset=0&start=2022-11-18T00:00&end=2022-11-20T00:00&filter=%7B%22PriceArea%22:[%22DK1%22]%7D&sort=HourDK%20ASC&timezone=dk`,
+  baseURL: `https://api.energidataservice.dk/dataset/Elspotprices?offset=0&start=${startDate}T00:00&end=${endDate}T00:00&filter=%7B%22PriceArea%22:[%22DK1%22]%7D&sort=HourDK%20ASC&timezone=dk`,
   withCredentials: false,
 });
 
